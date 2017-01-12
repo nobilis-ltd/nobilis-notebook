17fc67b09b95965a7f87ab0badb8a8202ba6d7ce
Harrisonkamau <kamauharrison87@gmail.com>
2017-01-12 09:14:49 +0300

    GET note_book

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,21 @@
 Rails.application.routes.draw do
-  get 'note_books/index'
 
-  get 'note_books/show'
-
-  get 'note_books/new'
-
-  get 'note_books/edit'
-
-  get 'notebooks/index'
 
   # For javascript tests
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
-  
+
   resources :users do
     resources :notes
+    resources :note_books
+
   end
 
   # Routes for Google auth
   get "auth/:provider/callback" => "sessions#create"
   get "auth/failure" => "sessions#auth_failure"
   get "signout" => "sessions#destroy", as: :signout
+  get "note_book" => "note_books#index"
+  get "/note_book/new" => "note_books#create"
 
   resources :sessions, only: [:create, :destroy]
 
