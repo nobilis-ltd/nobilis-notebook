3fb565314cefd172d4dcd63955633d609e689a56
Harrisonkamau <kamauharrison87@gmail.com>
2017-01-12 18:17:12 +0300

    create has_many association with notes

--- app/models/note_book.rb
+++ app/models/note_book.rb
@@ -3,10 +3,17 @@
 # Table name: note_books
 #
 #  id         :integer          not null, primary key
+#  name       :string
+#  user_id    :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_note_books_on_user_id  (user_id)
 #
 
 class NoteBook < ActiveRecord::Base
   belongs_to :user
+  has_many :notes
 end
