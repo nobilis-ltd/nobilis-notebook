e9a3fc371fc7c7b91f5c32e3c410a903cb034f63
Harrisonkamau <kamauharrison87@gmail.com>
2017-01-12 18:10:54 +0300

    modify note_book schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111084735) do
+ActiveRecord::Schema.define(version: 20170112132350) do
 
   create_table "note_books", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "note_books", ["user_id"], name: "index_note_books_on_user_id"
 
   create_table "notes", force: :cascade do |t|
     t.text     "content"
     t.string   "title"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "note_book_id"
   end
 
   add_index "notes", ["user_id"], name: "index_notes_on_user_id"
