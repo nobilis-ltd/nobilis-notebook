73728bb0c7d096504866a3c9982e04f0193d98ee
Harrisonkamau <kamauharrison87@gmail.com>
2017-02-06 10:49:37 +0300

    initialize Paloma

--- app/assets/javascripts/note_books/notes.js
+++ app/assets/javascripts/note_books/notes.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// initialize the module in the following actions
+Paloma.controller('NoteBooks/Notes', {
+  new: function(){
+    markdownEditorModule.init();
+  },
+  create: function(){
+    markdownEditorModule.init();
+  },
+  edit: function(){
+    markdownEditorModule.init();
+  },
+  show: function(){
+    noteShowPageRenderer.init(this.params.noteContent);
+  },
+  update: function(){
+    markdownEditorModule.init();
+  }
+})
